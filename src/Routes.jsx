5bb7b1130f03ef1f71dc253594636236aa89f620
kagwiandre <kagwiandrew@gmail.com>
2021-01-23 17:34:29 +0300

    fetching jobs and displaying in cards

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components';
-import JobDetail from './components/JobDetail';
+import JobDetail from './components/pages/JobDetail';
 
 function Routes() {
 	return (
---
+++ src/client.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const client = axios.create({
+	// baseURL: 'https://jobs.github.com',
+	headers: {
+		Accepts: 'application/json',
+		'Content-Type': 'application/json',
+	},
+});
+
+client.interceptors.response.use(res => {
+	if (res.status >= 200 && res.status < 300) return res.data;
+	throw new Error(res);
+});
+
+export default client;
