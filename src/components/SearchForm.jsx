e94d98767e0bbf5f55ca051f18f733c443ce5502
kagwiandre <kagwiandrew@gmail.com>
2021-01-28 13:37:53 +0300

    splited the search form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,10 +1,72 @@
-import React from 'react';
+import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+import {
+	Form,
+} from 'react-bootstrap';
+import { FaSearchMinus, FaSearchPlus } from 'react-icons/fa';
+import cn from 'classnames';
 
 function SearchForm() {
+	const [open, setOpen] = useState(false);
+
 	return (
-		<div>
-fuommm
-		</div>
+		<>
+			<div className={cn('form-wrapper', open && 'active')}>
+				<Form className="search-form">
+					<Form.Group>
+						<Form.Label className="form-label">Location</Form.Label>
+						<Form.Control placeholder="Where?" type="text" name="location" className="custom-checkbox" />
+					</Form.Group>
+					<Form.Group>
+						<Form.Label className="form-label">Type</Form.Label>
+						<div className="custom-checkbox text-center">
+							{/* <Row> */}
+							<Form.Check
+								custom
+								inline
+								type="radio"
+								label="Full Time"
+								name="fulltime"
+								id="fulltime"
+							/>
+							<Form.Check
+								custom
+								inline
+								type="radio"
+								label="Part Time"
+								name="parttime"
+								id="parttime"
+							/>
+
+							{/* </Row> */}
+						</div>
+					</Form.Group>
+				</Form>
+				<div className="mobile-search d-md-none">
+					<Button
+						onClick={() => setOpen(prevOpen => !prevOpen)}
+						variant="secondary"
+						size="sm"
+					>
+
+						{open ? <FaSearchMinus /> : <FaSearchPlus />}
+
+					</Button>
+				</div>
+
+			</div>
+			<div className="mobile-search d-md-none">
+				<Button
+					onClick={() => setOpen(prevOpen => !prevOpen)}
+					variant="secondary"
+					size="sm"
+				>
+
+					{open ? <FaSearchMinus /> : <FaSearchPlus />}
+
+				</Button>
+			</div>
+		</>
 	);
 }
 
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,15 +1,15 @@
 /* eslint-disable max-len */
-import Button from 'react-bootstrap/Button';
 import React, { useState, useEffect } from 'react';
 import {
-	Badge, Form, Image, Pagination,
+	Badge, Image, Pagination,
 } from 'react-bootstrap';
-import { FaSearchMinus, FaSearchPlus } from 'react-icons/fa';
+
 import { FcOvertime } from 'react-icons/fc';
 import { GoLocation } from 'react-icons/go';
 import { Link } from 'react-router-dom';
-import cn from 'classnames';
+
 import client from '../client';
+import SearchForm from './SearchForm';
 
 const baseURL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json';
 
@@ -34,7 +34,6 @@ function Home() {
 	useEffect(() => {
 		client.get(baseURL).then(setJobs);
 	}, []);
-	const [open, setOpen] = useState(false);
 
 	return (
 		<>
@@ -42,61 +41,8 @@ function Home() {
 				<div className="main-body">
 					<h2>Search For Developer Jobs</h2>
 					<div className="main-card">
-						<div className={cn('form-wrapper', open && 'active')}>
-							<Form className="search-form">
-								<Form.Group>
-									<Form.Label className="form-label">Location</Form.Label>
-									<Form.Control placeholder="Where?" type="text" name="location" className="custom-checkbox" />
-								</Form.Group>
-								<Form.Group>
-									<Form.Label className="form-label">Type</Form.Label>
-									<div className="custom-checkbox text-center">
-										{/* <Row> */}
-										<Form.Check
-											custom
-											inline
-											type="radio"
-											label="Full Time"
-											name="fulltime"
-											id="fulltime"
-										/>
-										<Form.Check
-											custom
-											inline
-											type="radio"
-											label="Part Time"
-											name="parttime"
-											id="parttime"
-										/>
+						<SearchForm />
 
-										{/* </Row> */}
-									</div>
-								</Form.Group>
-							</Form>
-							<div className="mobile-search d-md-none">
-								<Button
-									onClick={() => setOpen(prevOpen => !prevOpen)}
-									variant="secondary"
-									size="sm"
-								>
-
-									{open ? <FaSearchMinus /> : <FaSearchPlus />}
-
-								</Button>
-							</div>
-
-						</div>
-						<div className="mobile-search d-md-none">
-							<Button
-								onClick={() => setOpen(prevOpen => !prevOpen)}
-								variant="secondary"
-								size="sm"
-							>
-
-								{open ? <FaSearchMinus /> : <FaSearchPlus />}
-
-							</Button>
-						</div>
 						<div className="pagination-part">
 							<Pagination>
 								{/* <Pagination.First /> */}
