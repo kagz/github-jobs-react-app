ecb715b3324f15e0cec3856dce106102b74f6a4f
mpalmr <mspalmer91@gmail.com>
2021-02-15 18:46:37 -0500

    get job details

--- src/components/pages/JobDetail.jsx
+++ src/components/pages/JobDetail.jsx
@@ -7,36 +7,42 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import client from '../../client';
 
 const MainBody = styled.div`
-color: #b7cbd9;
-margin-top: 70px;
-min-height: 90vh;
-border-radius: 5px;`;
+	color: #b7cbd9;
+	margin-top: 70px;
+	min-height: 90vh;
+	border-radius: 5px;
+`;
+
 const CompanyLogo = styled(Card.Img)`
-height: 80px;
-width: 80px;
-border-radius: 50%;
-box-shadow: 5px 5px 10px rgba(0,0,0,0.5);
-margin: 6px;
+	height: 80px;
+	width: 80px;
+	border-radius: 50%;
+	box-shadow: 5px 5px 10px rgba(0,0,0,0.5);
+	margin: 6px;
 `;
+
 const JobCard = styled(Card)`
-padding: 20px;
-border-radius: 5px;
-background: #100e17;
-min-height: 90vh;
-box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
-margin: 15px;
+	padding: 20px;
+	border-radius: 5px;
+	background: #100e17;
+	min-height: 90vh;
+	box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
+	margin: 15px;
 `;
+
 const JobType = styled.div`
-display:flex;
+	display:flex;
 `;
+
 dayjs.extend(relativeTime);
-const baseURL = 'http://localhost:8010/proxy';
+
 function JobDetail() {
 	const [job, setJob] = useState(null);
-
 	const { jobId } = useParams();
+
 	useEffect(() => {
-		client.get(`${baseURL}/${jobId}.json`).then(setJob);
+		client.get(`/positions/${jobId}.json`)
+			.then(res => setJob(res.data));
 	}, []);
 
 	return job === null ? (
